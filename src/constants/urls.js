a98d4f508bfcd37941e00d24b828f4a1c21b0991
Souvikns <souvikde.ns@gmail.com>
2020-07-09 19:13:05 +0530

    route constants created

--- src/constants/urls.js
+++ src/constants/urls.js
@@ -2,3 +2,7 @@ export const BASE_URL = (process.env.NODE_ENV === 'development' ? 'http://localh
 
 export const LOGIN_URL = BASE_URL + '/auth/login'
 export const SIGNUP_URL = '/auth/signup'
+
+export const FILES_URL = BASE_URL + '/files'
+export const FILE_URL = FILES_URL + '/file'
+export const FOLDER_URL = FILES_URL + '/folder'
