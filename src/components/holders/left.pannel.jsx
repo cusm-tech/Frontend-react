0bd17a2af0f6ecbd66d887ec17d9e982d75bfe9a
Souvik <souvikde.ns@gmail.com>
2020-07-29 19:50:04 +0530

    added file upload ui

--- src/components/holders/left.pannel.jsx
+++ src/components/holders/left.pannel.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, { useCallback } from 'react'
+import { useDropzone } from 'react-dropzone'
 
 import {
     makeStyles,
@@ -9,7 +10,31 @@ import {
     Divider
 } from '@material-ui/core'
 
+const useStyle = makeStyles(theme => ({
+    fileUploadArea: {
+        border: '1px solid black',
+        padding: theme.spacing(1),
+        borderRadius: '6px'
+    },
+}))
+
+/**
+ * Upload file
+ * create folder
+ */
+
 const LeftPannel = props => {
+    const classes = useStyle()
+
+    const onDrop = useCallback(acceptedFiles => {
+        // Do something with the files
+        console.log(acceptedFiles[0])
+        const data = new FormData()
+        data.append('doc', acceptedFiles[0])
+
+    }, [])
+
+    const { getRootProps, getInputProps, isDragActive } = useDropzone({ onDrop })
 
     return (
         <div>
@@ -38,10 +63,15 @@ const LeftPannel = props => {
 
                 <Grid item xs={12}>
                     <FormControl fullWidth>
-                        <TextField
-                            variant="outlined"
-                            type="file"
-                        />
+                        <div {...getRootProps()} className={classes.fileUploadArea}>
+                            <input {...getInputProps()} />
+                            {
+                                isDragActive ?
+                                    <p>Drop the files here ...</p> :
+                                    <p>Drag 'n' drop some files here, or click to select files</p>
+                            }
+
+                        </div>
                     </FormControl>
                 </Grid>
 
